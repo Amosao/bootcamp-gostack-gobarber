be83d45c9656335e0745793420a1ba4a9c302f58
Amós Albuquerque <amosjunior_jr@hotmail.com>
2020-02-07 11:33:23 -0300

    chore: profile picture upload
    feat: multer config and functions

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,26 @@
+// import yup from 'yup';
+import User from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+  // LIST ALL PROVIDERS
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: {
+        provider: true,
+      },
+      attributes: ['id', 'name', 'email'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+    });
+
+    res.json(providers);
+  }
+}
+
+export default new ProviderController();
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import jwtAuth from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+// ROUTES WITHOUT AUTHENTICATION
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// ROUTES WITH AUTHENTICATION
 routes.use(jwtAuth);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
